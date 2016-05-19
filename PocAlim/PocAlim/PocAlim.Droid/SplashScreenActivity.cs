67a60069545d17229ba73f858a492e24723c3c59
JadBerro <jad.berro@sogeti.com>
2016-05-19 11:04:37 +0200

    Slpashscreen

---
+++ PocAlim/PocAlim/PocAlim.Droid/SplashScreenActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using System.Threading.Tasks;
+
+namespace PocAlim.Droid
+{
+	[Activity (Theme = "@style/MyTheme.Splash", MainLauncher = true, Icon = "@drawable/splash_logo")]
+	public class SplashScreenActivity : AppCompatActivity
+    {
+        static readonly string TAG = "X:" + typeof(SplashScreenActivity).Name;
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            Task startupWork = new Task(() => {
+                Task.Delay(5000);  // Simulate a bit of startup work.
+            });
+
+            startupWork.ContinueWith(t => {
+                StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+
+            startupWork.Start();
+        }
+    }
+}
+
+
